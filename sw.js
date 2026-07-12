7d7317c90ef482e4157a36d95161c3be12de1116
小山堅斗 <issyanomakku@gmail.com>
2026-07-13 00:57:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var SW_BUILD_VERSION = '2026-07-12-016';
+var SW_BUILD_VERSION = '2026-07-13-003';
 importScripts('./js/common/pwa/cache-config.js?v=' + SW_BUILD_VERSION);
 
 var config = self.MESOROGIA_PWA_CACHE_CONFIG;
